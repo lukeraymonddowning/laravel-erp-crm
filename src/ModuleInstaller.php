3ce74e46ae915ec2bfeb5d5621473e72c19fd5a9
Steve McDougall <juststevemcd@gmail.com>
2021-07-15 21:38:11 +0100

    wip

--- src/ModuleInstaller.php
+++ src/ModuleInstaller.php
@@ -7,35 +7,24 @@ namespace JustSteveKing\Laravel\ERP\CRM;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use JustSteveKing\Laravel\ERPContracts\Module\InstallerContract;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
 use Throwable;
 
 class ModuleInstaller implements InstallerContract
 {
     public function install(): void
     {
-        try {
-            Artisan::call(
-                command: 'vendor:publish',
-                parameters: [
-                             'provider' => 'JustSteveKing\Laravel\ERP\CRM\CRMServiceProvider',
-                             'tag' => 'laravel-erp-crm-migrations',
-                         ],
-            );
-        } catch (Throwable $exception) {
-            throw $exception;
-        }
+        $process = new Process([
+            (new ExecutableFinder)->find(
+                name: 'php',
+                default: 'php',
+            ),
+            'artisan',
+            'vendor:publish --provider="JustSteveKing\Laravel\ERP\CRM\CRMServiceProvider" --tag="laravel-erp-crm-migrations"'
+        ]);
 
-        try {
-            Artisan::call(
-                command: 'vendor:publish',
-                parameters: [
-                             'provider' => 'JustSteveKing\Laravel\ERP\CRM\CRMServiceProvider',
-                             'tag' => 'laravel-erp-crm-config',
-                         ],
-            );
-        } catch (Throwable $exception) {
-            throw $exception;
-        }
+        $process->mustRun();
 
         Log::info(
             message: 'Installing module juststeveking/laravel-erp-crm',

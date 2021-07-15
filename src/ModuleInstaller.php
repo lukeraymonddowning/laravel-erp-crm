3d795c2602c400d2f6fabccafb7bb06ca377763d
Steve McDougall <juststevemcd@gmail.com>
2021-07-15 21:28:43 +0100

    Building up the install script to publish migrations and config

--- src/ModuleInstaller.php
+++ src/ModuleInstaller.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace JustSteveKing\Laravel\ERP\CRM;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use JustSteveKing\Laravel\ERPContracts\Module\InstallerContract;
 
@@ -11,6 +12,21 @@ class ModuleInstaller implements InstallerContract
 {
     public function install(): void
     {
+        Artisan::call(
+            command: 'vendor:publish',
+            parameters: [
+                'provider' => 'JustSteveKing\Laravel\ERP\CRM\CRMServiceProvider',
+                'tag' => 'laravel-erp-crm-migrations',
+            ],
+        );
+
+        Artisan::call(
+            command: 'vendor:publish',
+            parameters: [
+                'provider' => 'JustSteveKing\Laravel\ERP\CRM\CRMServiceProvider',
+                'tag' => 'laravel-erp-crm-config',
+            ],
+        );
         Log::info(
             message: 'Installing module juststeveking/laravel-erp-crm',
         );
